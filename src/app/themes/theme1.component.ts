05da74c6bf515a58c1befe792387a38949212fb2
NagendraDevara <nagendradevara4@gmail.com>
2023-09-03 20:29:37 +0530

    initial js pdf

--- src/app/themes/theme1.component.ts
+++ src/app/themes/theme1.component.ts
@@ -1,3 +1,4 @@
+import { jsPDF } from 'jspdf';
 import { Component } from "@angular/core";
 import { Router } from "@angular/router";
 
@@ -5,13 +6,22 @@ import { Router } from "@angular/router";
 
 @Component({
     selector: 'app-component-theme',
-    templateUrl:'./theme1.component.html',
+    templateUrl: './theme1.component.html',
     styleUrls: ['./theme1.component.scss']
 })
-export class theme1{
-resumeSummary: any;
-constructor(private router: Router){
-    const navigation = this.router.getCurrentNavigation();
-    this.resumeSummary = navigation?.extras?.state;
-}
+export class theme1 {
+    resumeSummary: any;
+    constructor(private router: Router) {
+        const navigation = this.router.getCurrentNavigation();
+        this.resumeSummary = navigation?.extras?.state;
+    }
+    async convertToPdf() {
+        console.log(document.getElementById('pdf-content'));
+
+        const pdfContentEl: any = document.getElementById('pdf-content');
+
+        const doc = new jsPDF();
+
+        await doc.html(pdfContentEl.innerHTML).save('test.pdf');
+    }
 }
